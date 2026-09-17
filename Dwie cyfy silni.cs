8184b5709cc31be5b59e0f393d3b6b082b46b2a0
Praktykant <praktykantjeden@outlook.com>
2026-09-17 10:50:54 +0200

    Dodano drugie zadanie

---
+++ Dwie cyfy silni.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int d = int.Parse(Console.ReadLine());
+
+            while (d-- > 0)
+            {
+                long n = long.Parse(Console.ReadLine());
+
+                switch (n)
+                {
+                    case 0:
+                    case 1: Console.WriteLine("0 1"); break;
+                    case 2: Console.WriteLine("0 2"); break;
+                    case 3: Console.WriteLine("0 6"); break;
+                    case 4: Console.WriteLine("2 4"); break;
+                    case 5: Console.WriteLine("2 0"); break;
+                    case 6: Console.WriteLine("2 0"); break;
+                    case 8: Console.WriteLine("2 0"); break;
+                    case 7: Console.WriteLine("4 0"); break;
+                    case 9: Console.WriteLine("8 0"); break;
+                    default: Console.WriteLine("0 0"); break;
+                }
+            }
+        }
+    }
