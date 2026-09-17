c47dcab2f0addf265df5304cf9bf0e6dd4f86071
Praktykant <praktykantjeden@outlook.com>
2026-09-17 09:40:25 +0200

    Dodano zadanie Liczby Pierwsze

---
+++ LiczbyPierwsze.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace zadanie1;
+
+class Program {
+
+    static void Main(string[] args) 
+    {
+        int t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            bool CzyLiczbaP = true;
+
+            int liczba = int.Parse(Console.ReadLine());
+
+            if (liczba == 0 || liczba == 1)
+            {
+                CzyLiczbaP = false;
+            }
+            else
+            {
+
+                for (int i = 2; i < liczba - 1; i++)
+                {
+                    if (liczba % i == 0)
+                    {
+                        CzyLiczbaP = false;
+                        break;
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+
+            }
+
+            if (CzyLiczbaP == false)
+            {
+                Console.WriteLine("NIE");
+            }
+            else if(CzyLiczbaP == true)
+            {
+                Console.WriteLine("TAK");
+
+            }
+        }
+
+        
+    }
+    
+}
