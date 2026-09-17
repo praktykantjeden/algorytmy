7e200f29fb5754e14abb8781f926d6d06a42d9e7
Praktykant <praktykantjeden@outlook.com>
2026-09-17 11:38:12 +0200

    Dodano trzecie zadanie

---
+++ Czy umiesz potęgować.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        while (t-- > 0)
+        {
+            string[] wejscie = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(wejscie[0]);
+            int b = int.Parse(wejscie[1]);
+
+            int c = a % 10;
+
+            if (b == 0)
+            {
+                Console.WriteLine(1);
+                continue;
+            }
+
+            int ile = b % 4;
+            if (ile == 0)
+            {
+                ile = 4;
+            }
+
+            int potega = (int)Math.Pow(c, ile);
+            int wynik = potega % 10;
+
+            Console.WriteLine(wynik);
+        }
+    }
+}
