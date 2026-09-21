fe4050ce935348c23c8b14704563079a6647539f
Praktykant <praktykantjeden@outlook.com>
2026-09-21 11:17:14 +0200

    Dodano dwa zadania

---
+++ PomiarPredkosci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        while (t-- > 0)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] measurements = new int[n];
+            int maxPossibleSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                measurements[i] = int.Parse(Console.ReadLine());
+                maxPossibleSum += measurements[i];
+            }
+
+            HashSet<int> currentSpeeds = new HashSet<int>();
+            currentSpeeds.Add(0); 
+
+            for (int i = 0; i < n; i++)
+            {
+                int diff = measurements[i];
+                HashSet<int> nextSpeeds = new HashSet<int>();
+
+                foreach (int speed in currentSpeeds)
+                {
+                    nextSpeeds.Add(speed + diff);
+                    nextSpeeds.Add(speed - diff);
+                }
+
+                currentSpeeds = nextSpeeds;
+            }
+
+            int minAbsSpeed = int.MaxValue;
+            foreach (int speed in currentSpeeds)
+            {
+                int abs = Math.Abs(speed);
+                if (abs < minAbsSpeed)
+                {
+                    minAbsSpeed = abs;
+                }
+            }
+
+            int maxAbsSpeed = maxPossibleSum;
+
+            Console.WriteLine($"{minAbsSpeed} {maxAbsSpeed}");
+        }
+    }
+}
---
+++ TCPUDP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int Z = int.Parse(Console.ReadLine());
+
+        while (Z-- > 0)
+        {
+            string[] first = Console.ReadLine().Split();
+            int k = int.Parse(first[0]);
+            int p = int.Parse(first[1]);
+
+            bool[,] reach = new bool[k + 1, k + 1];
+
+            for (int i = 1; i <= k; i++)
+                reach[i, i] = true;
+
+            for (int i = 0; i < p; i++)
+            {
+                string[] edge = Console.ReadLine().Split();
+                int a = int.Parse(edge[0]);
+                int b = int.Parse(edge[1]);
+
+                reach[a, b] = true;
+            }
+
+            // Floyd-Warshall
+            for (int mid = 1; mid <= k; mid++)
+                for (int from = 1; from <= k; from++)
+                    if (reach[from, mid])
+                        for (int to = 1; to <= k; to++)
+                            if (reach[mid, to])
+                                reach[from, to] = true;
+
+            int z = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < z; i++)
+            {
+                string[] query = Console.ReadLine().Split();
+                int a = int.Parse(query[0]);
+                int b = int.Parse(query[1]);
+
+                if (!reach[a, b])
+                    Console.WriteLine("0");
+                else if (reach[b, a])
+                    Console.WriteLine("TCP");
+                else
+                    Console.WriteLine("UDP");
+            }
+        }
+    }
+}
